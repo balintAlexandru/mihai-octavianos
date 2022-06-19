18b66ef68c945c1aa8ead9fc090de4a04fa9cb70
Balint Alexandru <balintalex737@gmail.com>
2022-06-19 14:27:40 +0300

    remove files

--- src/utils/Styles/variables.js
+++ src/utils/Styles/variables.js
@@ -4,24 +4,11 @@ export const whitePrimaryColor = "#FDFDFD";
 export const secondaryColor = "#A7B4C0";
 export const darkSecondaryColor = "#050505";
 
-export const accentsColor = "#64B7FF";
-
-export const errorColor = "#b00000";
-export const confirmColor = "#25C23F";
-
 // font props
 export const regular = 400;
 export const semiBold = 600;
 export const bold = 700;
-export const titleFont = "Open Sans, sans-serif";
-export const iconPickerFont = "Cabin, sans-serif";
 
-export const categoryFontSize = "12px";
-
-// card container
-export const topBoxShadow = "4px -4px 4px -5px rgba(5, 5, 5, 0.4)";
-export const rightBoxShadow = "4px 1px 4px -5px rgba(5, 5, 5, 0.4)";
-export const bottomBoxShadow = "4px 4px 4px -5px rgba(5, 5, 5, 0.4)";
 
 // device size
 // export const mobile = "599px";
@@ -29,17 +16,4 @@ export const bottomBoxShadow = "4px 4px 4px -5px rgba(5, 5, 5, 0.4)";
 // export const tabletHigh = "1279px";
 // export const desktop = "1280px";
 
-// components section
-export const fullSectionWidth = "340px";
-export const dragSectionWidth = "40px";
-export const editSectionWidth = "300px";
 
-export const gapBetweenIcons = "10px";
-
-export const horizontalPadding = "15px";
-export const verticalPadding = "10px";
-
-export const lightThinnerLineBorder = `1px solid ${secondaryColor}`;
-export const thinnerLineBorder = `1px solid ${darkSecondaryColor}`;
-export const thinLineBorder = `2px solid ${darkSecondaryColor}`;
-export const regularBorderRadius = "3px";
