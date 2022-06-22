f1302ad8c1ea7a1bef5fa2a20c4f48ddd02a77f7
Balint Alexandru <balintalex737@gmail.com>
2022-06-23 00:57:44 +0300

    add redux toolkit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// REDUX 
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
