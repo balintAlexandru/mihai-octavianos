f03eb1792efd1baa9a18a858d445177f99a6e424
Balint Alexandru <balintalex737@gmail.com>
2022-07-23 18:53:12 +0300

    add structure

--- src/view/Archive/Archive.jsx
+++ src/view/Archive/Archive.jsx
@@ -5,11 +5,13 @@ import {
   changeBackgroundColor,
   enableScroll,
 } from "../../helpers/globalFunctions";
+import ARCHIVE from "../../constants/archive";
 
 // STYLE
 import * as Styled from "./ArchiveStyle";
 import * as variables from "../../utils/Styles/variables";
 
+console.log(ARCHIVE);
 const Archive = () => {
   useEffect(() => {
     changeBackgroundColor(variables.whitePrimaryColor);
@@ -18,7 +20,7 @@ const Archive = () => {
   return (
     <Styled.ArchiveContainer>
       <Styled.ArchiveImageWrapper>
-        <Styled.ArchiveImage src="" alt="" />
+        <Styled.ArchiveImage src={ARCHIVE[0].image} alt="picture" />
       </Styled.ArchiveImageWrapper>
       <Styled.ArchiveTitleWrapper></Styled.ArchiveTitleWrapper>
     </Styled.ArchiveContainer>
--- src/view/Archive/ArchiveStyle.jsx
+++ src/view/Archive/ArchiveStyle.jsx
@@ -8,11 +8,23 @@ import * as variables from "../../utils/Styles/variables";
 export const ArchiveContainer = styled.div`
   width: 100%;
   height: 100vh;
+  padding-top: 150px;
 `;
 
 // images
-export const ArchiveImageWrapper = styled.div``;
-export const ArchiveImage = styled.img``;
+export const ArchiveImageWrapper = styled.div`
+  width: 100%;
+  padding: 0 30px;
+`;
+export const ArchiveImage = styled.img`
+  width: 100%;
+  height: 100%;
+`;
 
 // title
-export const ArchiveTitleWrapper = styled.div``;
+export const ArchiveTitleWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 15px;
+  overflow-x: scroll;
+`;
